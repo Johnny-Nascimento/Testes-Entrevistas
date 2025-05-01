69b76f3912a54efe3e200972291e307d0522f805
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-04-30 21:43:05 -0300

    - Adicionado projeto de testes, adicionado testes iniciais com maximo de 8 de tamanho.
    TODO: testar erros e performance quando lista for muito grande.

---
+++ Logica_Benner_Test/Logica_Benner_Test/Test1.cs
@@ -0,0 +1,177 @@
+﻿using Teste_Logica;
+
+namespace Logica_Benner_Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Deve_Retornar_Apenas_True()
+        {
+            // Arrange
+
+            Arvore arvore = new Arvore(8);
+
+            // Act
+
+            arvore.Connect(1, 2);
+            arvore.Connect(1, 6);
+            arvore.Connect(6, 2);
+            arvore.Connect(2, 4);
+            arvore.Connect(5, 8);
+
+            // Assert
+
+            Assert.IsTrue(arvore.Query(1, 2));
+            Assert.IsTrue(arvore.Query(1, 6));
+            Assert.IsTrue(arvore.Query(6, 2));
+            Assert.IsTrue(arvore.Query(2, 4));
+            Assert.IsTrue(arvore.Query(5, 8));
+        }
+
+        [TestMethod]
+        public void Deve_Retornar_Apenas_Falso()
+        {
+            // Arrange
+
+            Arvore arvore = new Arvore(8);
+
+            // Act
+
+            arvore.Connect(1, 2);
+            arvore.Connect(1, 6);
+            arvore.Connect(6, 2);
+            arvore.Connect(2, 4);
+            arvore.Connect(5, 8);
+
+            // Assert
+
+            Assert.IsFalse(arvore.Query(7, 1));
+            Assert.IsFalse(arvore.Query(7, 2));
+            Assert.IsFalse(arvore.Query(7, 3));
+            Assert.IsFalse(arvore.Query(7, 4));
+            Assert.IsFalse(arvore.Query(7, 5));
+            Assert.IsFalse(arvore.Query(7, 6));
+            Assert.IsFalse(arvore.Query(7, 8));
+
+            Assert.IsFalse(arvore.Query(3, 1));
+            Assert.IsFalse(arvore.Query(3, 2));
+            Assert.IsFalse(arvore.Query(3, 4));
+            Assert.IsFalse(arvore.Query(3, 5));
+            Assert.IsFalse(arvore.Query(3, 6));
+            Assert.IsFalse(arvore.Query(3, 7));
+            Assert.IsFalse(arvore.Query(3, 8));
+        }
+
+        [TestMethod]
+        public void Deve_Retornar_Apenas_Niveis_De_Conexao_0()
+        {
+            // Arrange
+
+            Arvore arvore = new Arvore(8);
+
+            // Act
+
+            // Assert
+
+            Assert.AreEqual(0, arvore.LevelConnection(1, 2));
+            Assert.AreEqual(0, arvore.LevelConnection(2, 3));
+            Assert.AreEqual(0, arvore.LevelConnection(3, 4));
+            Assert.AreEqual(0, arvore.LevelConnection(4, 5));
+            Assert.AreEqual(0, arvore.LevelConnection(6, 7));
+            Assert.AreEqual(0, arvore.LevelConnection(7, 8));
+        }
+
+        [TestMethod]
+        public void Deve_Retornar_Apenas_Niveis_De_Conexao_1()
+        {
+            // Arrange
+
+            Arvore arvore = new Arvore(8);
+
+            // Act
+
+            arvore.Connect(1, 2);
+            arvore.Connect(3, 4);
+            arvore.Connect(6, 7);
+
+            // Assert
+
+            Assert.AreEqual(1, arvore.LevelConnection(1, 2));
+            Assert.AreEqual(1, arvore.LevelConnection(3, 4));
+            Assert.AreEqual(1, arvore.LevelConnection(6, 7));
+        }
+
+        [TestMethod]
+        public void Deve_Retornar_Apenas_Niveis_De_Conexao_2()
+        {
+            // Arrange
+
+            Arvore arvore = new Arvore(8);
+
+            // Act
+
+            arvore.Connect(1, 2);
+            arvore.Connect(2, 3);
+            arvore.Connect(3, 4);
+            arvore.Connect(4, 5);
+            arvore.Connect(5, 6);
+            arvore.Connect(6, 7);
+            arvore.Connect(7, 8);
+
+            // Assert
+
+            Assert.AreEqual(2, arvore.LevelConnection(1, 3));
+            Assert.AreEqual(2, arvore.LevelConnection(2, 4));
+            Assert.AreEqual(2, arvore.LevelConnection(3, 5));
+            Assert.AreEqual(2, arvore.LevelConnection(4, 6));
+            Assert.AreEqual(2, arvore.LevelConnection(5, 7));
+            Assert.AreEqual(2, arvore.LevelConnection(6, 8));
+            Assert.AreEqual(2, arvore.LevelConnection(8, 6));
+        }
+
+        [TestMethod]
+        public void Deve_Retornar_Falso_Para_Os_Desconectados()
+        {
+            // Arrange
+
+            Arvore arvore = new Arvore(8);
+
+            arvore.Connect(1, 6);
+            arvore.Connect(5, 8);
+
+            // Act
+
+            arvore.Disconnect(1, 6);
+            arvore.Disconnect(5, 8);
+
+
+            // Assert
+
+            Assert.IsFalse(arvore.Query(1, 6));
+            Assert.IsFalse(arvore.Query(5, 8));
+        }
+
+        [TestMethod]
+        public void Deve_Testar_O_Tempo_Para_Achar_O_Ultimo_LevelConnection()
+        {
+            // Arrange
+
+            Arvore arvore = new Arvore(8);
+
+            arvore.Connect(1, 6);
+            arvore.Connect(5, 8);
+
+            // Act
+
+            arvore.Disconnect(1, 6);
+            arvore.Disconnect(5, 8);
+
+
+            // Assert
+
+            Assert.IsFalse(arvore.Query(1, 6));
+            Assert.IsFalse(arvore.Query(5, 8));
+        }
+    }
+}
---
+++ Teste Logica Benner/Teste Logica Benner/Arvore.cs
@@ -0,0 +1,155 @@
+﻿namespace Teste_Logica
+{
+    // The class should also provide four public methods, Connect, Disconnect, Query and LevelConnection.
+    public class Arvore
+    {
+        private Dictionary<int, List<int>> folhas = new Dictionary<int, List<int>>();
+
+        // The constructor should take a positive integer value indicating the number of elements in the set. Passing 
+        // an invalid value should throw an exception.
+        public Arvore(int quantidadeFolhas)
+        {
+            if (quantidadeFolhas <= 0)
+                throw new Exception("Esperado número maior que 0.");
+
+            for (int i = 1; i <= quantidadeFolhas; i++)
+            {
+                folhas.Add(i, new List<int>());
+            }
+        }
+
+        // The connect method will take two integers indicating the elements to connect.This method should throw exceptions as appropriate.
+        public void Connect(int folhaA, int folhaB)
+        {
+            ValidaParametros(folhaA, folhaB);
+
+            List<int> conexoesA = new List<int>();
+            folhas.TryGetValue(folhaA, out conexoesA);
+
+            List<int> conexoesB = new List<int>();
+            folhas.TryGetValue(folhaB, out conexoesB);
+
+            if (conexoesA.Count != 0 && conexoesB.Count != 0)
+            {
+                int conexao = conexoesA.FindIndex(c => c == folhaB);
+                if (conexao != -1)
+                    throw new Exception("(A) ja está conectado com (B).");
+            }
+
+            conexoesA.Add(folhaB);
+            conexoesB.Add(folhaA);
+        }
+
+        // The disconnect method will take two integers indicating the elements to disconnect.This method should throw exceptions as appropriate.
+        public void Disconnect(int folhaA, int folhaB)
+        {
+            ValidaParametros(folhaA, folhaB);
+
+            List<int> conexoesA = new List<int>();
+            folhas.TryGetValue(folhaA, out conexoesA);
+
+            List<int> conexoesB = new List<int>();
+            folhas.TryGetValue(folhaB, out conexoesB);
+
+            int conexao = conexoesA.FindIndex(c => c == folhaB);
+            if (conexao == -1)
+                throw new Exception("Elementos não estão conectados entre si.");
+
+            conexoesA.Remove(folhaB);
+            conexoesB.Remove(folhaA);
+        }
+
+        // The query method will take two integers indicating the elements to query.This method should throw exceptions as appropriate.
+        // It should return true if the elements are connected, directly or indirectly, and
+        // false if the elements are not connected.
+        public bool Query(int folhaA, int folhaB)
+        {
+            ValidaParametros(folhaA, folhaB);
+
+            List<int> conexoesA = new List<int>();
+            folhas.TryGetValue(folhaA, out conexoesA);
+
+            List<int> conexoesB = new List<int>();
+            folhas.TryGetValue(folhaB, out conexoesB);
+
+            if (conexoesA.Count == 0 || conexoesB.Count == 0)
+                return false;
+
+            int conexao = conexoesA.FindIndex(c => c == folhaB);
+            if (conexao == -1)
+                return false;
+
+            return true;
+        }
+
+        // The levelConnetion method will also take two integers and should also throw an exception as appropriate, returning an integer value.
+        // It should return 0 if the elements are not connected, 1 if the elements are
+        // directly connected and 2 or more when elements are indirectly connect, returning the number that
+        // represents how many connections there are between the searched elements. 
+        public int LevelConnection(int folhaA, int folhaB)
+        {
+            ValidaParametros(folhaA, folhaB);
+
+            List<int> conexoesA = new List<int>();
+            folhas.TryGetValue(folhaA, out conexoesA);
+
+            List<int> conexoesB = new List<int>();
+            folhas.TryGetValue(folhaB, out conexoesB);
+
+            int conexao = conexoesA.FindIndex(c => c == folhaB);
+            if (conexao != -1)
+                return 1;
+
+            var folhasAux = folhas;
+            int numeroConexoes = 1;
+            List<int> conexoesAux = new List<int> { folhaA };
+            while (true)
+            {
+                numeroConexoes++;
+
+                folhasAux = folhasAux.Where(
+                    l => l.Value.FindIndex(c => conexoesAux.FindIndex(cc => cc == c) != -1) != -1
+                    ).ToDictionary();
+
+                if (folhasAux.Count == 0)
+                    return 0;
+
+                conexoesAux.Clear();
+
+                foreach (var folha in folhasAux)
+                {
+                    conexoesAux.Add(folha.Key);
+
+                    int index = folha.Value.FindIndex(c => c == folhaB);
+                    if (index != -1)
+                        return numeroConexoes;
+                }
+            }
+
+            return 0;
+        }
+
+        private void ValidaParametros(int folhaA, int folhaB)
+        {
+            if (folhaA == folhaB)
+                throw new Exception("Esperado números diferentes.");
+
+            ValidaDiferenteDeZero(folhaA, folhaB);
+            ValidaExiste(folhaA, folhaB);
+        }
+
+        private void ValidaDiferenteDeZero(int folhaA, int folhaB)
+        {
+            if (folhaA <= 0 || folhaB <= 0)
+                throw new Exception("Esperado número maior que 0.");
+        }
+
+        private void ValidaExiste(int folhaA, int folhaB)
+        {
+            List<int> _ = new List<int>();
+
+            if (!folhas.TryGetValue(folhaA, out _) || !folhas.TryGetValue(folhaB, out _))
+                throw new Exception("Uma ou ambas as posições não existem.");
+        }
+    }
+}
